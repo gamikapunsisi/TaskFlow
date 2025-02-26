0192fd6fdb7ca0948ab28c709836eb907e6a4159
supun basura <sbasura27@gmail.com>
2025-02-26 16:55:48 +0530

    Initial Commit

---
+++ UniversityN/UniversityNApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniversityNApp.swift
+//  UniversityN
+//
+//  Created by Supun Basura on 2025-02-26.
+//
+
+import SwiftUI
+
+@main
+struct UniversityNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UniversityNUITests/UniversityNUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  UniversityNUITestsLaunchTests.swift
+//  UniversityNUITests
+//
+//  Created by Supun Basura on 2025-02-26.
+//
+
+import XCTest
+
+final class UniversityNUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
